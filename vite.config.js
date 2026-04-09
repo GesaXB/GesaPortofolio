5b62a5e9b2eff863fbca417583bc7c16079eb696
GesaXb <gesavlop@gmail.com>
2026-04-10 01:35:32 +0700

    fix: double compatibility for GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  // Automatically use the correct base path for GitHub Pages OR Vercel
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? '/GesaPortofolio/' : '/',
 })
